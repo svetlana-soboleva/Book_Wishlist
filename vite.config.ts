e53c402c4a4cf19d4ab6df0208335e6bda2b83ee
Svetlana Soboleva <132843403+svetlana-soboleva@users.noreply.github.com>
2024-10-29 08:46:48 +0100

    ✨ change the base path to Book_Wishlist

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), TanStackRouterVite()],
-  base: "/",
+  base: "/Book_Wishlist/",
 });
